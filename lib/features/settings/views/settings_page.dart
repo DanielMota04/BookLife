ef47c82f902006a989093ba12a0d1e138d652879
Daniel <Dmota5705@gmail.com>
2026-05-12 09:38:08 -0300

    Base da tela de ajustes criada
    Criada também a tela de temas (base) e adicionada uma rota para ela

--- lib/features/settings/views/settings_page.dart
+++ lib/features/settings/views/settings_page.dart
@@ -1,11 +1,50 @@
+import 'package:book_life/app/router/routes.dart';
 import 'package:book_life/core/widgets/app_scaffold.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(body: const Center(child: Text('Ajustes')));
+    return CustomScaffold(
+      body: Column(
+        children: [
+          SettingsItem(text: 'Editar Perfil', onTap: () => context.go(Routes.editProfile)),
+          SettingsItem(text: 'Alterar Senha', onTap: () => context.go(Routes.changePassword)),
+          SettingsItem(text: 'Limpar Cache', onTap: () => {}), // implementar função de limpar cache depois
+          SettingsItem(text: 'Temas', onTap: () => context.go(Routes.themes)),
+          SettingsItem(text: 'Sobre', onTap: () => context.go(Routes.about)),
+          SettingsItem(text: 'Sair', onTap: () => {
+            // implementar logout quando tiver pronto
+            context.go(Routes.welcome)
+          })
+        ],
+      )
+    );
+  }
+}
+
+
+class SettingsItem extends StatelessWidget {
+  const SettingsItem({super.key, required this.text, required this.onTap});
+
+  final String text;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(width: 15),
+          Text(text),
+        ],
+      ),
+    );
   }
 }
