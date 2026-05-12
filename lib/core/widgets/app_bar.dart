0ce771440de4760ed8c2016c3927430bdc177aec
Daniel <Dmota5705@gmail.com>
2026-05-12 08:43:17 -0300

    Criado widget do header do sistema

---
+++ lib/core/widgets/app_bar.dart
@@ -0,0 +1,28 @@
+import 'package:book_life/core/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: AppColors.steelBlue,
+      leading: Builder(
+        builder: (context) => IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () => Scaffold.of(context).openDrawer(),
+        ),
+      ),
+      actions: [
+        IconButton(
+          icon: const Icon(Icons.account_circle_outlined),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+}
