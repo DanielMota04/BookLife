59749c114af605d20a394b665004e54995c0354a
Daniel <Dmota5705@gmail.com>
2026-05-12 11:51:54 -0300

    Model de book alterado

--- lib/core/enums/reading_status.dart
+++ lib/core/enums/reading_status.dart
@@ -1,1 +1,9 @@
 enum ReadingStatus { reading, completed, wishlist }
+
+extension ReadingStatusLabel on ReadingStatus {
+  String get displayName => switch (this) {
+    ReadingStatus.reading => 'Lendo',
+    ReadingStatus.completed => 'Lido',
+    ReadingStatus.wishlist => 'Em espera',
+  };
+}
