187a76a7a87dfe7e4826913852312cc85530ee1a
Lobo <davilobo004@gmail.com>
2026-05-13 10:21:05 -0300

    Ajuste para rota Welcome

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:book_life/features/auth/views/login_page.dart';
 import 'package:book_life/features/auth/views/register_page.dart';
+import 'package:book_life/features/auth/views/welcome_page.dart';
 import 'package:book_life/features/library/views/biblioteca.dart';
 import 'package:book_life/features/library/views/cadastrar_livro.dart';
 import 'package:book_life/features/progress/views/meu_progresso.dart';
@@ -20,6 +21,10 @@ final appRouter = GoRouter(
     return null;
   },
   routes: [
+     GoRoute(
+      path: Routes.welcome,
+      builder: (context, state) => const WelcomePage(),
+    ),
     GoRoute(
       path: Routes.login,
       builder: (context, state) => const LoginPage(),
