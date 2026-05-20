4ac09c1f89c37e2d1a499514acc948bd18c8ec26
DanielMota04 <dmota5705@gmail.com>
2026-05-20 17:48:49 -0300

    Ajuste nas rotas

--- lib/app/router/app_router.dart
+++ lib/app/router/app_router.dart
@@ -1,16 +1,9 @@
-import 'package:book_life/features/auth/repositories/auth_repository.dart';
-import 'package:book_life/features/auth/viewmodels/auth_viewmodel.dart';
 import 'package:book_life/features/book_details/views/livro_details.dart';
 import 'package:book_life/features/auth/views/welcome_page.dart';
 import 'package:book_life/features/library/views/biblioteca.dart';
 import 'package:book_life/features/library/views/cadastrar_livro.dart';
 import 'package:book_life/features/progress/views/meu_progresso.dart';
-import 'package:book_life/features/settings/viewmodels/change_password_viewmodel.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:go_router/go_router.dart';
-import 'package:book_life/features/goals/views/metas.dart';
-import 'package:provider/provider.dart';
 
 import '../../features/settings/views/settings_page.dart';
 import '../../features/settings/views/edit_profile_page.dart';
@@ -20,37 +13,17 @@ import '../../features/settings/views/about_page.dart';
 import 'routes.dart';
 
 final appRouter = GoRouter(
-  initialLocation: Routes.welcome,
+  initialLocation: Routes
+      .welcome,
   redirect: (context, state) {
     // implementar verificação de autenticação quando o auth ja estiver configurado
     return null;
   },
   routes: [
-    GoRoute(
+     GoRoute(
       path: Routes.welcome,
-      builder: (context, state) => MultiProvider(
-
-        providers: [
-
-          ChangeNotifierProvider(
-            create: (context) => LoginViewModel(
-              AuthRepository(FirebaseAuth.instance, FirebaseFirestore.instance),
-            ),
-          ),
-          
-          ChangeNotifierProvider(
-            create: (context) => RegisterViewModel(
-              AuthRepository(FirebaseAuth.instance, FirebaseFirestore.instance),
-            ),
-          ),
-        ],
-
-        child: const WelcomePage(),
-
-      ),
+      builder: (context, state) => const WelcomePage(),
     ),
-
-    // settings
     GoRoute(
       path: Routes.settings,
       builder: (context, state) => const SettingsPage(),
@@ -61,12 +34,7 @@ final appRouter = GoRouter(
     ),
     GoRoute(
       path: Routes.changePassword,
-      builder: (context, state) => ChangeNotifierProvider(
-        create: (_) => ChangePasswordViewModel(
-          AuthRepository(FirebaseAuth.instance, FirebaseFirestore.instance),
-        ),
-        child: const ChangePasswordPage(),
-      ),
+      builder: (context, state) => const ChangePasswordPage(),
     ),
     GoRoute(path: Routes.about, builder: (context, state) => const AboutPage()),
     GoRoute(
@@ -74,7 +42,6 @@ final appRouter = GoRouter(
       builder: (context, state) => const ThemesPage(),
     ),
 
-    // library
     GoRoute(
       path: Routes.library,
       builder: (context, state) => const MinhaBiblioteca(),
@@ -83,19 +50,16 @@ final appRouter = GoRouter(
       path: Routes.addBook,
       builder: (context, state) => const AdicionarLivroPage(),
     ),
-    GoRoute(
-      path: '${Routes.library}/:id',
+  GoRoute(
+      path: '${Routes.library}/:id', 
       builder: (context, state) {
         final idLivro = state.pathParameters['id'] ?? '1';
-        return LivroDetails(bookId: idLivro);
-      },
+        return LivroDetails(bookId: idLivro,); 
+      }
     ),
-
-    // progress
     GoRoute(
       path: Routes.progress,
       builder: (context, state) => const MeuProgressoPage(),
     ),
-    GoRoute(path: Routes.goals, builder: (context, state) => const MetasPage()),
   ],
 );
