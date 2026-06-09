a9c0948b094ce4873930787d0be3a96d8cf461cf
Daniel <Dmota5705@gmail.com>
2026-06-09 09:44:41 -0300

    imagem de perfil adicionada

--- lib/features/settings/repositories/profile_repository.dart
+++ lib/features/settings/repositories/profile_repository.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'dart:typed_data';
 import 'package:book_life/core/errors/auth_errors.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -8,7 +10,11 @@ class ProfileRepository {
 
   ProfileRepository(this._firestore, this._auth);
 
-  Future<void> updateData({String? username, String? email}) async {
+  Future<void> updateData({
+    String? username,
+    String? email,
+    Uint8List? photoBytes,
+  }) async {
     final user = _auth.currentUser;
     if (user == null) throw UserNotLoggedInException();
 
@@ -21,6 +27,7 @@ class ProfileRepository {
         'updatedAt': DateTime.now(),
         if (username != null && username.isNotEmpty) 'nome': username,
         if (email != null && email.isNotEmpty) 'email': email,
+        if (photoBytes != null) 'photoBase64': base64Encode(photoBytes),
       };
 
       await _firestore.collection('users').doc(user.uid).update(data);
